4f8479f786ab60e057a51dfd211e02f90e105dc2
Juan Cruz <tonocruz9000@gmail.com>
2026-02-20 21:36:22 -0700

    Feat: fixed the root file

--- root.js
+++ root.js
@@ -1,44 +1,16 @@
-console.log("Front-end app.js online");
-
 const express = require('express');
-const path = require('path');
 const app = express();
+
+const frontendApp = require('./frontend/app'); 
+const backendApp = require('./backend/app');
+
+
+app.use('/api', backendApp);
+app.use('', frontendApp);
+
 const PORT = process.env.PORT || 3050;
-//Works locally, I am unsure if this will work when we deploy
-const API_URL = `http://localhost:${PORT}/api/`
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-app.get("/", (req, res) =>{
-    res.render('homepage');
+app.listen(PORT, () => {
+    console.log(`InventoryTrackerPlus running on port ${PORT}`);
+    console.log(`API & Docs: http://localhost:${PORT}/api`);
+    console.log(`Frontend: http://localhost:${PORT}/`);
 });
-app.get("/contact-us", (req, res) =>{
-    res.render('contact-us')
-});
-app.get("/inventory", async (req, res) =>{
-    //GET THIS RUNNING!!
-    console.log("(FRONT-END) GETTING ALL STOCK");
-    const url = `${API_URL}stock`;
-    try{
-        fetch(url)
-        .then(res => res.json())
-        .then (data =>{
-            console.log(data);
-            let model = {inventoryList: data.body};
-            res.render("view-stock", model);
-        });
-    }catch(err){
-        console.error("Error fetching stock data:", err);
-        res.status(500).send("Error fetching stock data");
-    }
-    
-});
-
-
-
-module.exports = app;
